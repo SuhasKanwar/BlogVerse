05589eadc5107f207a29c6ad6924ad6eb4b150f7
SuhasKanwar <suhas.kanwar@gmail.com>
2024-12-01 13:38:38 +0530

    Sorted comments by post date

--- controllers/blogViewController.js
+++ controllers/blogViewController.js
@@ -3,7 +3,7 @@ const Comment = require('../models/comments');
 
 exports.blogViewRender = async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
-    const comments = await Comment.find({ blogID: req.params.id }).populate('createdBy');
+    const comments = await Comment.find({ blogID: req.params.id }).sort({ createdAt: -1 }).populate('createdBy');
     return res.render('blogView', {
         user: req.user,
         blog: blog,
