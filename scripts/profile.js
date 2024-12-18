2aeb6c2a0e7ecf83792bfc622eb832e43b11b352
SuhasKanwar <suhas.kanwar@gmail.com>
2024-12-18 23:47:30 +0530

    Trying to fix the profile UI

--- scripts/profile.js
+++ scripts/profile.js
@@ -19,3 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function calculateAge(birthDate) {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDifference = today.getMonth() - birth.getMonth();
+    
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+    
+    return age;
+  }
