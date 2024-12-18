71c8c6bc38dba6725c15bdc698d5078b2905b66e
SuhasKanwar <suhas.kanwar@gmail.com>
2024-12-18 13:20:50 +0530

    Halfway through the updation process of profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,33 +1,113 @@
-const mongoose = require('mongoose');
-const Blog = require('../models/blogs');
-const Comment = require('../models/comments');
-const Users = require('../models/users');
+const mongoose = require("mongoose");
+const Blog = require("../models/blogs");
+const Comment = require("../models/comments");
+const Users = require("../models/users");
+const fs = require("fs");
+const path = require("path");
 
 exports.profileRender = async (req, res) => {
-    const profileID = req.params.id;
+  const profileID = req.params.id;
 
-    if (!req.user) {
-        return res.redirect('/login');
+  if (!req.user) {
+    return res.redirect("/login");
+  }
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(profileID)) {
+      return res.status(400).send("Invalid profile ID");
     }
 
-    try {
-        if (!mongoose.Types.ObjectId.isValid(profileID)) {
-            return res.status(400).send('Invalid profile ID');
+    const userBlogs = await Blog.find({ createdBy: profileID }).sort({
+      createdAt: -1,
+    });
+
+    const userComments = await Comment.find({ createdBy: profileID })
+      .populate("blogID")
+      .sort({ createdAt: -1 });
+
+    const user = await Users.findById(profileID);
+
+    return res.render("profile", {
+      user: user,
+      blogs: userBlogs,
+      comments: userComments,
+      currUser: req.user,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
+  }
+};
+
+exports.profileEditRednder = async (req, res) => {
+  const profileID = req.params.id;
+  if (req.user._id.toString() === profileID.toString()) {
+    const user = await Users.findById(profileID);
+    return res.render("profile-edit", {
+      user: user,
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
+
+exports.profileUpdateHandler = async (req, res) => {
+  try {
+    const {
+      fullName,
+      email,
+      bio,
+      dateOfBirth,
+      gender,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+      github,
+    } = req.body;
+
+    let profileImageURL = req.body.profileImageURL;
+    if (req.file) {
+      profileImageURL = `/uploads/${req.file.filename}`;
+    }
+
+    const user = await Users.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    if (req.file && user.profileImageURL !== '/avatars/default-avatar.png') {
+      const oldAvatarPath = path.join(__dirname, '../public', user.profileImageURL);
+      fs.unlink(oldAvatarPath, (err) => {
+        if (err) {
+          console.error("Error deleting old avatar:", err);
+        } else {
+          console.log("Old avatar deleted successfully");
         }
-
-        const userBlogs = await Blog.find({ createdBy: profileID }).sort({ createdAt: -1 });
-        
-        const userComments = await Comment.find({ createdBy: profileID }).populate('blogID').sort({ createdAt: -1 });
-
-        const user = await Users.findById(profileID);
-
-        return res.render('profile', {
-            user: user,
-            blogs: userBlogs,
-            comments: userComments
-        });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send('Server Error');
+      });
     }
+
+    // Update the user's fields
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.bio = bio || user.bio;
+    user.dateOfBirth = dateOfBirth || user.dateOfBirth;
+    user.gender = gender || user.gender;
+    user.socials = {
+      facebook: facebook || user.socials.facebook,
+      twitter: twitter || user.socials.twitter,
+      instagram: instagram || user.socials.instagram,
+      linkedin: linkedin || user.socials.linkedin,
+      github: github || user.socials.github,
+    };
+    user.profileImageURL = profileImageURL;
+
+    await user.save();
+
+    res.redirect(`/profile/${user._id}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
 
 const router = express.Router();
 
 const profileController = require('../controllers/profileController');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve('./public/avatars'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
 router.get('/:id', profileController.profileRender);
+router.get('/edit/:id', profileController.profileEditRednder);
+router.patch('/edit/:id', upload.single('profileImage'), profileController.profileUpdateHandler);
 
 module.exports = router;
